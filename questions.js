f0bb2c359c96ca59776307c9fde84f6eb803bddc
Leah Hamilton <leahrhamilton@yahoo.com>
2019-03-27 13:25:30 -0400

    initial push includes html files, images, and css.

---
+++ questions.js
@@ -0,0 +1,38 @@
+const DATA = [
+    {
+        question: "Argentinian by birth, he played a crucial part in the Cuban revolution, as well as in communist activities in other Latin American countries. Who is he?"
+        ,
+        answers: [
+            "Emiliano Zapata",
+            "Carlos Menem",
+            "Fidel Castro",
+            "Che Guevara"
+        ],
+        correctAnswer: "Ernesto Che Guevara (1928-1967) was born into a middle-class family in Rosario, Argentina. He received a medical degree from the University of Buenos Aires in 1953. In 1954 he decided to go to Mexico and join Cuban revolutionaries under Fidel Castro.",
+        image: "images\CheGuevara.jpg",
+        alt: "Image of Che Guevara"
+    },
+    {
+        questions: "Which from the following, is an Argentinian national drink?",
+        answers: [
+            "Quilmes",
+            "Guaraná",
+            "Mate",
+            "Tango", 
+        ],
+        correctAnswer: "Mate is Argentina's national drink. Yerba mate is a tea prepared most traditionally in a gourd. The infused yerba mate herbs are not strained out from the water, but instead are sipped through a 'bombille' that acts both as straw and sieve. Yerba mate is prepared in one gourd that is shared; this is part of the social and cultural experience of drinking yerba mate.",
+        image: "images\Yerba-Mate.jpg",
+        alt: "Yerba Mate"
+    },
+    {
+        questions: "What is the 'milonga'?",
+        answers: [
+            "Quilmes",
+            "Guaraná",
+            "Mate",
+            "Tango", 
+        ],
+        image: "images\Yerba-Mate.jpg",
+        alt: "Yerba Mate"
+    }
+]
