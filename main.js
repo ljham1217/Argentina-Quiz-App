e2f802537c8842880ff4e64bb8b90177af7d7ea9
Leah Hamilton <leahrhamilton@yahoo.com>
2019-04-03 07:11:55 -0400

    Implemented Wireframe, includes one index.html and completed jquery

---
+++ main.js
@@ -0,0 +1,163 @@
+'use strict'
+
+let questionNumber = 0;
+let score=0; 
+
+
+function startQuiz(){
+    //this function will hide the start div and display the form div
+    $('.startbutton').on('click', function (){
+        $('.quizStart').hide();
+        $('.questionAnswerForm').css('display','block');
+        $('.questionNumber').text(1);
+    });
+}
+
+function generateQuestion() {
+    //this function will display the question and answer options
+    if (questionNumber < questionsArray.length) {
+        return `<form><fieldset>    
+           
+            <legend><h1>${questionsArray[questionNumber].question}</h1></legend>
+            <p style="margin-bottom: 20px"><span class="alert hidden"> Please make a selection below:</span></p> 
+            <label for="answer1" class="radioContainer">${questionsArray[questionNumber].answers[0]}
+            <input type="radio" id="answer1" value="${questionsArray[questionNumber].answers[0]}" name="answer">
+            <span class="circle"></span></label><br>
+
+            <label for="answer2" class="radioContainer">${questionsArray[questionNumber].answers[1]}
+            <input type="radio" id="answer2" value="${questionsArray[questionNumber].answers[1]}" name="answer">
+            <span class="circle"></span></label><br>
+
+            <label for="answer3" class="radioContainer ontop">${questionsArray[questionNumber].answers[2]}
+            <input type="radio" id="answer3" value="${questionsArray[questionNumber].answers[2]}" name="answer">
+            <span class="circle"></span></label><br>
+
+            <label for="answer4" class="radioContainer">${questionsArray[questionNumber].answers[3]}
+            <input type="radio" id="answer4" value="${questionsArray[questionNumber].answers[3]}" name="answer">
+            <span class="circle"></span></label><br>    
+            <button type="button" class="submitbutton">Submit</button> 
+        </fieldset></form>   
+        `;
+    }
+}
+
+//render the questions Array in the DOM
+function renderQuiz(){
+        $('.questionAnswerForm').html(generateQuestion());
+}
+
+/*function enableSubmitButton(){
+    $('input[name=answer]').on('click', function (event){
+        $('.submitbutton').prop('disabled', false);
+    })
+}*/
+function submitQuizAnswer() {
+    $('.questionAnswerForm').on('click', '.submitbutton', function(event) {
+        event.preventDefault();
+        $('.score').text(score);
+        compareAnswer();
+    });
+}
+
+
+function compareAnswer() {
+    let selected= $('input[name=answer]:checked').val();
+    console.log(selected);
+    let rightChoice = questionsArray[questionNumber].correctAnswer;
+        if (selected === rightChoice) {
+            correctView();
+            incrementQuestion();
+            quizScore();
+        } else if ($('input[name=answer]:checked').length === 0) {
+            $('.questionAnswerForm').find('span').removeClass('hidden');
+        } else if (selected !== rightChoice) {
+            wrongView();
+            incrementQuestion();
+        }
+}
+
+function correctView(){
+    $('.questionAnswerForm').hide();
+    $('.answerForm').css('display','flex');
+    $('.answerForm').html(
+        `<img src="${questionsArray[questionNumber].image}" alt="${questionsArray[questionNumber].alt}">
+        <h1>Correct!</h1>
+        <p>${questionsArray[questionNumber].description}</p>    
+        <button type="button" class="nextQuestion">Next</button>`
+    );
+
+}
+
+function wrongView(){
+    $('.questionAnswerForm').hide();
+    $('.answerForm').css('display','flex');
+    $('.answerForm').html(
+        `<img src="${questionsArray[questionNumber].image}" alt="${questionsArray[questionNumber].alt}">
+        <h1>Sorry, you got it wrong.</h1>
+        <p>${questionsArray[questionNumber].description}</p>    
+        <button type="button" class="nextQuestion">Next</button>`
+    );
+}
+
+function generateNextQuestion() {
+    //this function will hide the start div and display the form div
+    $('.answerForm').on('click', '.nextQuestion', function (event){
+        event.preventDefault();
+        if (questionNumber < questionsArray.length) {
+        $('.answerForm').hide();
+        $('.questionAnswerForm').css('display','block');
+        $('.questionAnswerForm').html(generateQuestion());
+        $('.questionNumber').text(questionNumber + 1);
+        } else {
+            if (score > 6) {
+            $('.answerForm').css('display','block');
+                highScore();
+                startNewQuiz();
+            } else {
+                lowScore();
+                startNewQuiz();
+            }
+        }      
+    });
+}
+
+function highScore() {
+    $('.answerForm').addClass('resultsPage').html(
+        `<h1>Congratulations! You're a pro!</h1>
+        <p>You got ${score} out of 9 answers.</p>
+        <button type="button" class="startbutton">Start New Quiz</button>`
+        );
+}
+
+function lowScore() {
+    $('.answerForm').addClass('resultsPage').html(
+        `<h1>You could learn more about Argentina.</h1>
+        <p>You got ${score} out of 9</p>
+        <button type="button" class="startbutton">Start New Quiz</button>`
+        );
+}
+
+function startNewQuiz(){
+    $('.answerForm').on('click', '.startbutton', function (event){
+        location.reload();
+    });
+}
+
+function incrementQuestion() {
+        questionNumber++;
+}
+
+function quizScore(){
+    /*increment the score.*/
+    score++;
+}
+
+function handleQuiz(){
+    startQuiz();
+    renderQuiz();
+    submitQuizAnswer();
+    generateNextQuestion();
+}
+
+//when the page loads call handleQuiz
+$(handleQuiz);
--- questions.js
+++ questions.js
@@ -1,38 +1,119 @@
-const DATA = [
+const questionsArray = [
     {
-        question: "Argentinian by birth, he played a crucial part in the Cuban revolution, as well as in communist activities in other Latin American countries. Who is he?"
-        ,
+        question: "Argentinian by birth, he played a crucial part in the Cuban revolution, as well as in communist activities in other Latin American countries. Who is he?",
         answers: [
             "Emiliano Zapata",
             "Carlos Menem",
             "Fidel Castro",
             "Che Guevara"
         ],
-        correctAnswer: "Ernesto Che Guevara (1928-1967) was born into a middle-class family in Rosario, Argentina. He received a medical degree from the University of Buenos Aires in 1953. In 1954 he decided to go to Mexico and join Cuban revolutionaries under Fidel Castro.",
-        image: "images\CheGuevara.jpg",
+        correctAnswer: "Che Guevara",
+        description: "Ernesto Che Guevara (1928-1967) was born into a middle-class family in Rosario, Argentina. He received a medical degree from the University of Buenos Aires in 1953. In 1954 he decided to go to Mexico and join Cuban revolutionaries under Fidel Castro.",
+        image: "images/CheGuevara.jpg",
         alt: "Image of Che Guevara"
     },
     {
-        questions: "Which from the following, is an Argentinian national drink?",
+        question: "Which from the following, is an Argentinian national drink?",
         answers: [
             "Quilmes",
             "Guaraná",
             "Mate",
-            "Tango", 
+            "Tango"
         ],
-        correctAnswer: "Mate is Argentina's national drink. Yerba mate is a tea prepared most traditionally in a gourd. The infused yerba mate herbs are not strained out from the water, but instead are sipped through a 'bombille' that acts both as straw and sieve. Yerba mate is prepared in one gourd that is shared; this is part of the social and cultural experience of drinking yerba mate.",
-        image: "images\Yerba-Mate.jpg",
+        correctAnswer: "Mate",
+        description: "Mate is Argentina's national drink. Yerba mate is a tea prepared most traditionally in a gourd. The infused yerba mate herbs are not strained out from the water, but instead are sipped through a 'bombille' that acts both as straw and sieve. Yerba mate is prepared in one gourd that is shared; this is part of the social and cultural experience of drinking yerba mate.",
+        image: "images/Yerba-Mate.jpg",
         alt: "Yerba Mate"
     },
     {
-        questions: "What is the 'milonga'?",
+        question: "What is the 'milonga'?",
         answers: [
-            "Quilmes",
-            "Guaraná",
-            "Mate",
-            "Tango", 
+            "A typical Argentine dish",
+            "A Tango dance party",
+            "A famous football stadium",
+            "A type of witchcraft widely used in Argentina" 
         ],
-        image: "images\Yerba-Mate.jpg",
-        alt: "Yerba Mate"
+        correctAnswer: "A Tango dance party",
+        description:"The milonga is a social dance party where tango is danced. This is a dance that developed in San Telmo and has European influence. This dance has come back into fashion and is being danced more frequently by younger generations (I can do the eight steps!).",
+        image: "images/Milonga.jpg",
+        alt: "Image of People Dancing Tango at a Milonga"
+    },
+    {
+        question: "In 1933, she wanted to become an actress. In 1951, she wanted to become the vice-president of Argentina. Her tomb can be found at Recoleta Cemetery in Buenos Aires.",
+        answers: [
+            "Eva Peron",
+            "Susana Gimenez",
+            "Chiche Duhalde",
+            "Mirtha Legrand" 
+        ],
+        correctAnswer: "Eva Peron",
+        description:"María Eva Duarte de Perón was the wife of Argentine President Juan Perón and First Lady of Argentina from 1946 until her death in 1952. She is usually referred to as Eva Perón or Evita. She was born in poverty in the rural village of Los Toldos, in the Pampas, as the youngest of five children.",
+        image: "images/EvaPeron.jpg",
+        alt: "Image of Eva Peron"
+    },
+    {
+        question: "Which from the following, is the national dish of Argentina?",
+        answers: [
+            "Milanesa",
+            "Dulce De Leche",
+            "Empanada",
+            "Asado" 
+        ],
+        correctAnswer: "Asado",
+        description:"Argentina is not a place for vegetarians, it is a place for those who love beef. The 'asado' (BBQ) is a traditional meal and is cooked on charcoal.",
+        image: "images/Asado.jpg",
+        alt: "Asado cooking over charcoal"
+    },
+    {
+        question: "Nicknamed 'El Pibe de Oro', he played 91 matches for the Argentine national team between 1977-1994. Who is he?",
+        answers: [
+            "Diego Maradona",
+            "Diego Simeone",
+            "Gabriel Batistuta",
+            "Carlos Valderrama" 
+        ],
+        correctAnswer: "Diego Maradona",
+        description:"Diego Armando Maradona is considered an idol by the Argentines. His vision, passing, ball control and dribbling skills was combined with his small stature (1.65 m or 5 ft 5 in), gave him a low center of gravity which allowed him to maneuver better than most other soccer players. A precocious talent, Maradona was given the nickname 'El Pibe de Oro' ('The Golden Boy').",
+        image: "images/Diego_Maradona.jpg",
+        alt: "Diego Maradona playing soccer"
+    },
+    {
+        question: "They´ve been called the wanderers of the Pampas. Who are they?",
+        answers: [
+            "Charros",
+            "Boleadoras",
+            "Gauchos",
+            "Kooris" 
+        ],
+        correctAnswer: "Gauchos",
+        description:"The gaucho is a national symbol in Argentina and Uruguay. They are the nomadic and colourful horseman and cowhand of the Argentine Pampas (grasslands), who flourished from the mid-18th to the mid-19th century and have remained a folk hero similar to the cowboy in western North America.",
+        image: "images/Gauchos.jpg",
+        alt: "Gauchos"
+    },
+    {
+        question: "What is the name of the most popular card game in Argentina?",
+        answers: [
+            "Solitario",
+            "Black Jack",
+            "Truco",
+            "Bridge" 
+        ],
+        correctAnswer: "Truco",
+        description:"Truco is widely played in Argentina. It is derived from the simpler game Truc, which is played in Catalonia and southern France. In contrast to Poker, where things are kept at a low level and quiet, the game of Truco is actually played by tricking your opponent by playing fast and distracting them through conversation. Truco is generally played with friends, and players are often loud, telling jokes, and talking amongst each other.",
+        image: "images/truco.png",
+        alt: "Truco Cards"
+    },
+    {
+        question: "Which of these Argentine provinces does not produce wine?",
+        answers: [
+            "Mendoza",
+            "Tierra Del Fuego",
+            "San Juan",
+            "La Rioja" 
+        ],
+        correctAnswer: "Tierra Del Fuego",
+        description:"Tierra del Fuego is too cold to produce wine!",
+        image: "images/TierraDelFuego.jpg",
+        alt: "Tierra Del Fuego"
     }
 ]
